850b4f9f1c6418425c17c7bd9e8734e7dadb9714
Rebecca Serna <becca@example.com>
2025-07-12 01:39:45 -0500

    use backend env variable

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
 // Connect to backend server
-const socket = io("http://localhost:3001");
+const socket = io("process.env.REACT_APP_BACKEND_URL");
+//comment
 
 function App() {
   const [name, setName] = useState("");
